e6e5ff88f01921eff0a73162f3c938c755091295
ppxb <317842449@qq.com>
2026-06-09 11:53:44 +0800

    chore: add tanstack router and plugin

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,9 +1,17 @@
 import type { PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default function createVitePlugins() {
-  const vitePlugins: (PluginOption | PluginOption[])[] = [react(), tailwindcss()]
+  const vitePlugins: (PluginOption | PluginOption[])[] = [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true
+    }),
+    react(),
+    tailwindcss()
+  ]
 
   return vitePlugins
 }
